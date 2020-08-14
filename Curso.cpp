e97657057215e8b265e0b94e94ee0a0d82d4e700
camisaenz510 <68917860+camisaenz510@users.noreply.github.com>
2020-08-14 15:22:38 -0600

    curso

--- Curso.cpp
+++ Curso.cpp
@@ -3,3 +3,74 @@
 //
 
 #include "Curso.h"
+
+Curso::Curso() {
+    this->code="";
+    this->name="";
+    this->date=0;
+    this->hour=0;
+    this->professor= nullptr;
+}
+
+Curso::Curso(std::string code,std::string name, int date, int hour, Profesor *professor) {
+    this->code=code;
+    this->name=name;
+    this->date=date;
+    this->hour=hour;
+    this->professor= professor;
+}
+
+std::string Curso::getCode() {
+    return this->code;
+}
+
+void Curso::setCode(std::string code) {
+    this->code=code;
+}
+
+int Curso::getHour() {
+    return this->hour;
+}
+
+int Curso::setHour(int hour) {
+    this->hour=hour;
+}
+
+int Curso::getDate() {
+    return this->date;
+}
+
+int Curso::setDate(int date) {
+    this->date=date;
+}
+
+Profesor Curso::getProfessor() {
+    return *professor;
+
+}
+
+void Curso::setProfessor(Profesor*professor) {
+    this->professor=professor;
+}
+
+std::string Curso::getName() {
+    return this->name;
+}
+
+void Curso::setName(std::string name) {
+    this->name=name;
+}
+
+std::string Curso::toString() {
+
+    std::stringstream s;
+    s << "\tName: " << getName() << std::endl;
+    s << "\tCode: " << getCode() << std::endl;
+    s << "\tDate: " << getDate() << std::endl;
+    s << "\tHour: " << getHour() << std::endl;
+    s << "\tProfessor: " <<professor<< std::endl;  //aca el getProfessor() me da error y no se por que :(
+    return s.str();
+    return std::string();
+}
+
+
--- Curso.h
+++ Curso.h
@@ -4,9 +4,32 @@
 
 #ifndef BASIC_00_EXAMPLE_CURSO_H
 #define BASIC_00_EXAMPLE_CURSO_H
-
+#include <sstream>
+#include <iomanip>
+#include "Profesor.h"
 
 class Curso {
+    std::string code;
+    std::string name;
+    int date;
+    int hour;
+    Profesor *professor;
+
+private:
+    Curso();
+    Curso(std::string code, std::string name, int date, int hour, Profesor*professor);
+    std::string getCode();
+    void setCode(std::string code);
+    int getHour();
+    int setHour(int hour);
+    int getDate();
+    int setDate(int date);
+    Profesor getProfessor();
+    void setProfessor(Profesor*professor);
+    std::string getName();
+    void setName(std::string name);
+    std::string toString();
+
 
 };
 
