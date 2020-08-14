e52b150ba9a287dd957e01a961ff46ebfb3437a8
camisaenz510 <68917860+camisaenz510@users.noreply.github.com>
2020-08-14 11:19:11 -0600

    persona

--- Persona.cpp
+++ Persona.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "Persona.h"
+
+Persona::Persona() {
+    this->name="";
+    this->id="";
+    this->age=0;
+}
+
+Persona::Persona(std::string name, std::string id, int age) {
+    this->name=name;
+    this->id=id;
+    this->age=age;
+}
+
+std::string Persona::getName() {
+    return this->name;
+}
+
+void Persona::setName(std::string name) {
+    this->name=name;
+}
+
+std::string Persona::getId() {
+    return this->id;
+}
+
+void Persona::setId(std::string id) {
+    this->id=id;
+}
+
+int Persona::getAge() {
+    return this->age;
+}
+
+void Persona::setAge(int age) {
+    this->age=age;
+}
--- Persona.h
+++ Persona.h
@@ -4,10 +4,24 @@
 
 #ifndef BASIC_00_EXAMPLE_PERSONA_H
 #define BASIC_00_EXAMPLE_PERSONA_H
-
+#include <sstream>
+#include <iomanip>
 
 class Persona {
+    std::string name;
+    std::string id;
+    int age;
 
+private:
+    Persona();
+    Persona(std::string name, std::string id, int age);
+    std::string getName();
+    void setName(std::string name);
+    std::string getId();
+    void setId(std::string id);
+    int getAge();
+    void setAge(int age);
+    std::string toString()const;
 };
 
 
